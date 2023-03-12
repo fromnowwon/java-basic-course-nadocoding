1751b18118d39d527f79b1a033970f11799d7efe
cw <fromnowwon@gmail.com>
2023-03-12 20:50:00 +0900

    feat: chap_03_03_StringCompare

---
+++ src/chap_03/_03_StringCompare.java
@@ -0,0 +1,24 @@
+package chap_03;
+
+public class _03_StringCompare {
+    public static void main(String[] args) {
+        // 문자열 비교
+        String s1 = "Java";
+        String s2 = "Python";
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s2.equalsIgnoreCase("python"));
+
+        // 문자열 비교 심화
+        s1 = "1234";
+        s2 = "1234";
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s1 == s2);
+
+        s1 = new String("1234");
+        s2 = new String("1234");
+        System.out.println(s1.equals(s2)); // true: 내용 비교
+        System.out.println(s1 == s2); // false: 참조 비교
+    }
+}
