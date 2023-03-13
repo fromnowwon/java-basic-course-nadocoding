af471a4e28deae2e20a088177bb1ca67724279ea
cw <fromnowwon@gmail.com>
2023-03-13 20:56:01 +0900

    feat: chap_05_02_ArrayLoop

---
+++ src/chap_05/_02_ArrayLoop.java
@@ -0,0 +1,29 @@
+package chap_05;
+
+public class _02_ArrayLoop {
+    public static void main(String[] args) {
+        // 배열의 순회
+        String[] coffees = {"아메리카노", "카페모카", "라떼", "바닐라라떼"};
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(coffees[i] + " 하나");
+        }
+        System.out.println("주세요.");
+
+        System.out.println("---------------------");
+
+
+        for (int i = 0; i < coffees.length; i++) {
+            System.out.println(coffees[i] + " 하나");
+        }
+        System.out.println("주세요.");
+
+        System.out.println("---------------------");
+
+
+        for (String coffee : coffees) {
+            System.out.println(coffee + " 하나");
+        }
+        System.out.println("주세요.");
+    }
+}
