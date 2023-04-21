39a53f4e2b152b0255a32fff654a09b56ff397a5
fromnowwon <fromnowwon@gmail.com>
2023-04-21 16:34:57 +0900

    feat: chap_08_02_Interface

--- src/chap_08/Camera/FactoryCam.java
+++
@@ -1,9 +0,0 @@
-package chap_08.Camera;
-
-public class FactoryCam extends Camera {
-
-    @Override
-    public void showMainFeature() {
-        System.out.println("화재 감지");
-    }
-}
--- src/chap_08/Camera/SpeedCam.java
+++
@@ -1,9 +0,0 @@
-package chap_08.Camera;
-
-public class SpeedCam extends Camera {
-
-    @Override
-    public void showMainFeature() {
-        System.out.println("속도 측정, 번호 인식");
-    }
-}
