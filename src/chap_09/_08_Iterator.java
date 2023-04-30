8b4d5c395f216999cbae0cc4cfa51ce848925354
cw <fromnowwon@gmail.com>
2023-04-30 21:18:31 +0900

    chap_09_08_Iterator

---
+++ src/chap_09/_08_Iterator.java
@@ -0,0 +1,98 @@
+package chap_09;
+
+import java.util.*;
+
+public class _08_Iterator {
+    public static void main(String[] args) {
+        // 이터레이터
+        List<String> list = new ArrayList<>();
+        list.add("유재석");
+        list.add("(알 수 없음)");
+        list.add("김종국");
+        list.add("(알 수 없음)");
+        list.add("강호동");
+        list.add("(알 수 없음)");
+        list.add("박명수");
+        list.add("(알 수 없음)");
+        list.add("조세호");
+
+        for (String s: list) {
+            System.out.println(s);
+        }
+
+        System.out.println("-------------");
+
+        Iterator<String> it = list.iterator();
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.next());
+
+        System.out.println("-------------");
+
+        it = list.iterator(); // 커서를 처음 위치로 이동
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        System.out.println("-------------");
+
+        it = list.iterator(); // 커서를 처음 위치로 이동
+
+        while (it.hasNext()) {
+            String s = it.next();
+            if (s.contains("(알 수 없음)")) {
+                it.remove();
+            }
+        }
+
+        it = list.iterator(); // 커서를 처음 위치로 이동
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        System.out.println("-------------");
+
+        HashSet<String> set = new HashSet<>();
+        set.add("유재석");
+        set.add("박명수");
+        set.add("강호동");
+
+        Iterator<String> itSet = set.iterator();
+
+        while (itSet.hasNext()) {
+            System.out.println(itSet.next());
+        }
+
+        System.out.println("-------------");
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("유재석", 10);
+        map.put("박명수", 5);
+
+        // map.iterator(); map에서는 바로 적용되지 않음
+
+        Iterator<String> iterMapKey = map.keySet().iterator();
+
+        while (iterMapKey.hasNext()) {
+            System.out.println(iterMapKey.next());
+        }
+
+        Iterator<Integer> iterMapValue = map.values().iterator();
+
+        while (iterMapValue.hasNext()) {
+            System.out.println(iterMapValue.next());
+        }
+
+        System.out.println("-------------");
+
+        Iterator<Map.Entry<String, Integer>> iterMap = map.entrySet().iterator();
+
+        while (iterMap.hasNext()) {
+            System.out.println(iterMap.next());
+        }
+
+    }
+}
