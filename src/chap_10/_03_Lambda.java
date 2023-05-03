e1d1335e3b8380454e051d36773018b7ea48c015
fromnowwon <fromnowwon@gmail.com>
2023-05-03 15:48:03 +0900

    chap_10_03_Lambda

---
+++ src/chap_10/_03_Lambda.java
@@ -0,0 +1,29 @@
+package chap_10;
+
+public class _03_Lambda {
+    public static void main(String[] args) {
+        // 람다식 (간결한 형태의 코드 뭉치)
+        // (파라미터1, 파라미터2, ...) -> { 코드 }
+    }
+
+//        public void print() {
+//            String s = "test";
+//            System.out.println(s);
+//        }
+
+
+//        () -> {
+//            String s = "test";
+//            System.out.println(s);
+//        }
+
+    // s -> System.out.println(s)
+
+    // (x, y) -> x + y
+
+    public void print() {
+        String s = "test";
+        System.out.println(s);
+    }
+
+}
